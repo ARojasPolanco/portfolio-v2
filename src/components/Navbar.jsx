ce8e10bea466804437d0e73b5d13a888cdf16df0
ARojasPolanco <advbrrop23@gmail.com>
2023-09-15 14:11:50 -0300

    Change title and favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,35 +13,35 @@ const Navbar = () => {
         <section>
             <section className='bg-bg-200/10 z-10 backdrop-blur-sm w-full p-2 fixed bottom-0 flex justify-between items-center sm:flex-col sm:top-0 sm:w-[50px] sm:rounded-full sm:my-auto sm:h-[250px] sm:right-4'>
                 <Link to='Home' smooth={true} duration={300}>
-                    <Tooltip title='Home' placement='left'>
+                    <Tooltip title='Inicio' placement='left'>
                         <IconButton>
                             <HomeIcon className='text-white hover:text-netlify hover:scale-125' />
                         </IconButton>
                     </Tooltip>
                 </Link>
                 <Link to='About' smooth={true} duration={300}>
-                    <Tooltip title='About Me' placement='left'>
+                    <Tooltip title='Sobre mí' placement='left'>
                         <IconButton>
                             <PersonIcon className='text-white hover:text-netlify hover:scale-125' />
                         </IconButton>
                     </Tooltip>
                 </Link>
                 <Link to='Skills' smooth={true} duration={300}>
-                    <Tooltip title='Skills' placement='left'>
+                    <Tooltip title='Habilidades' placement='left'>
                         <IconButton>
                             <SchoolIcon className='text-white hover:text-netlify hover:scale-125' />
                         </IconButton>
                     </Tooltip>
                 </Link>
                 <Link to='Portfolio' smooth={true} duration={300}>
-                    <Tooltip title='Projects' placement='left'>
+                    <Tooltip title='Proyectos' placement='left'>
                         <IconButton>
                             <DashboardIcon className='text-white hover:text-netlify hover:scale-125' />
                         </IconButton>
                     </Tooltip>
                 </Link>
                 <Link to='Contact' smooth={true} duration={300}>
-                    <Tooltip title='Contact Me' placement='left'>
+                    <Tooltip title='Contacto' placement='left'>
                         <IconButton>
                             <ContactPageIcon className='text-white hover:text-netlify hover:scale-125' />
                         </IconButton>
